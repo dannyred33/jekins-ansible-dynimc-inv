bfa94469cbf5e0ebd11859405429323dd77b21f2
dannyred33 <119339378+dannyred33@users.noreply.github.com>
2023-02-10 14:31:23 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,12 +1,12 @@
 provider "aws"{
-  region = "ap-southeast-2"
+  region = "ap-south-1"
 }
 
 
 resource "aws_instance" "AWSInstance"{
-    ami = "ami-08cd0547bf85331e7"
+    ami = "ami-OeO7dcaca348aOe68"
 	instance_type = "t2.micro"
-	key_name = "devopstrainingmss"
+	key_name = "DineshTech"
 	security_groups = ["launch-wizard-2"]
 	tags = {
 	 Name = "tomcatservers"
